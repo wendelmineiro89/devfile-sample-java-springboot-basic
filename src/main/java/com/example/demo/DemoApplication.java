9951023920d10093b5d7ec4feb45e9a051840764
Wendel Ferreira <wendel.ferreira@ccee.org.br>
2022-12-05 14:11:35 -0300

    demo 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello Oficina do Saber 1!";
     }
 
     public static void main(String[] args) {
